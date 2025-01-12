e2c0d502a2ffbf0a11c24cde2e92317ce9446743
Kaushik-Iyer <kushlaser@gmail.com>
2025-01-11 20:37:22 -0500

    creating new webpages. header and footer init. correcting price values being displayed

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "home#index"
-
+  resources :players, only: [:index]
   get 'teams/standings', to: 'teams#standings'
   get 'players/top_performers', to: 'players#top_performers'
   get 'fixtures/upcoming', to: 'fixtures#upcoming'
